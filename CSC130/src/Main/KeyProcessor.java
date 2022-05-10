74a1080cd16644c98f7d3e258425deb8738e4dba
Ahmed Osman <ahmedaosman00@gmail.com>
2022-05-10 12:36:05 -0700

    Checkpoint #5

--- CSC130/src/Main/KeyProcessor.java
+++ CSC130/src/Main/KeyProcessor.java
@@ -1,4 +1,5 @@
 /* This will handle the "Hot Key" system. */
+/* This will handle the "Hot Key" system. */
 
 package Main;
 
@@ -9,7 +10,7 @@ public class KeyProcessor{
 	// Static Fields
 	private static char last = ' ';			// For debouncing purposes
 	private static stopWatchX sw = new stopWatchX(250);
-	
+
 	// Static Method(s)
 	public static void processKey(char key){
 		if(key == ' ')				return;
@@ -18,17 +19,33 @@ public class KeyProcessor{
 			if(sw.isTimeUp() == false)			return;
 		last = key;
 		sw.resetWatch();
-		
+
 		/* TODO: You can modify values below here! */
 		switch(key){
-		case '%':								// ESC key
-			System.exit(0);
-			break;
-			
-		case 'm':
-			// For mouse coordinates
-			Control.isMouseCoordsDisplayed = !Control.isMouseCoordsDisplayed;
-			break;
+			case '%':								// ESC key
+				System.exit(0);
+				break;
+
+			case 'w':
+				Main.trigger = "w is triggered";
+				break;
+
+			case 'a':
+				Main.trigger = "a is triggered";
+				break;
+
+			case 's':
+				Main.trigger = "s is triggered";
+				break;
+
+			case 'd':
+				Main.trigger = "d is triggered";
+				break;
+
+			case 'm':
+				// For mouse coordinates
+				Control.isMouseCoordsDisplayed = !Control.isMouseCoordsDisplayed;
+				break;
 		}
 	}
 }
