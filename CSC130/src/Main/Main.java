0da196f73853152939a94ae32080bc1d067fa739
Ahmed Osman <ahmedaosman00@gmail.com>
2022-05-10 12:28:33 -0700

    Initial Commit

--- CSC130/src/Main/Main.java
+++ CSC130/src/Main/Main.java
@@ -1,33 +1,70 @@
 // Name: 				Ahmed Osman
-// Due Date:		 	Initial Commit
+// Assignment:		 	Checkpoint #3
 // Course: 				CSC 130 Spring 2022
 
 package Main;
 
-import java.awt.Color;
+import java.util.ArrayList;
+import Data.Vector2D;
+import Data.spriteInfo;
 import logic.Control;
+import timer.stopWatchX;
 
 public class Main{
-	// Fields (Static) below...
-	public static Color c = new Color(36, 28, 255);
-	// End Static fields...
+	/* Fields (Static) below... */
+
+	public static stopWatchX timer = new stopWatchX(85); /* Set timer here (milliseconds) */
+	public static Vector2D currentVec = new Vector2D(-128, 296); /* To hold temporary 2DVector Position */
+	public static int currentSpriteIndex = 0; /* Points to index in sprites ArrayList */
+	public static int frameCounter = 0; /* To hold reference to current png */
+	public static spriteInfo currentSprite = new spriteInfo(currentVec, "stick"+frameCounter); /* To hold temporary spriteInfo */
+	public static ArrayList<spriteInfo> sprites = new ArrayList<>(); /* Holds all sprite animation frames and vector positions */
+
+	/* Fields Not Currently in Use
+	public static Queue<Vector2D> vecs1 = new LinkedList<>();
+	public static Queue<Vector2D> vecs2 = new LinkedList<>();
+	public static boolean isImageDrawn = false;
+	public static Color c = new Color(255, 255, 51);
+	*/
+
+	/* End Static fields... */
 
 	public static void main(String[] args) {
-		Control ctrl = new Control();				// Do NOT remove!
-		ctrl.gameLoop();							// Do NOT remove!
+		Control ctrl = new Control();				/* Do NOT remove! */
+		ctrl.gameLoop();							/* Do NOT remove! */
 	}
 
 	/* This is your access to things BEFORE the game loop starts */
-	public static void start(){
-		// TODO: Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite or drawString)
+	public static void start() {
 
+		/* Populate array with new spriteInfo Objects */
+		while (currentVec.getX() <= 1280){
+			if (frameCounter >= 8){
+				frameCounter = 0;
+			}
+			sprites.add(new spriteInfo(new Vector2D(currentVec.getX(), currentVec.getY()), "stick"+frameCounter));
+			currentVec.adjustX(16); /* Controls Degree of Sprite Movement */
+			frameCounter++;
+		}
 	}
+
 
 	/* This is your access to the "game loop" (It is a "callback" method from the Control class (do NOT modify that class!))*/
 	public static void update(Control ctrl) {
-		// TODO: This is where you can code! (Starting code below is just to show you how it works)
-		ctrl.addSpriteToFrontBuffer(390, 400, "f0");						 				// Add a tester sprite to render list by tag (Remove later! Test only!)
-		ctrl.drawString(390, 400, "My name is Ahmed", c);		// Test drawing text on screen where you want (Remove later! Test only!)
+
+		ctrl.addSpriteToFrontBuffer(currentSprite.getCoords().getX(), currentSprite.getCoords().getY(), currentSprite.getTag());
+
+		/* Time-dependent array iteration */
+		if(timer.isTimeUp()){
+			currentSprite = sprites.get(currentSpriteIndex);
+			currentSpriteIndex++;
+			if (currentSpriteIndex == (sprites.size() - 1)){ /* Restart Array Pointer */
+				currentSpriteIndex = 0;
+			}
+			timer.resetWatch(); /* Adjust timer in fields */
+		}
+
+
 	}
 
 	// Additional Static methods below...(if needed)
